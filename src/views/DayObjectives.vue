cd790c290bdd3b8e93a6623ef286eec13064a3d3
Jérémy Legendre <jeremy.legendre@raccourci.fr>
2021-03-16 20:15:16 +0100

    day objectives default format

--- src/views/DayObjectives.vue
+++ src/views/DayObjectives.vue
@@ -1,5 +1,5 @@
 <template>
-  <ion-page>
+  <ion-page class="today-objectives">
     <ion-header>
       <ion-toolbar>
         <ion-title>Objectifs du jour</ion-title>
@@ -11,24 +11,33 @@
           <ion-title size="large">Objectifs du jour</ion-title>
         </ion-toolbar>
       </ion-header>
-      <ion-list>
-        <div v-for="n in 24" :key="n">
-          {{ n }}
+      <div class="date">
+        {{today}}
+      </div>
+      <ion-list v-if="activities">
+        <div class="objective" v-for="activity in activities" :key="activity.id">
+          <div class="hour"> {{ activity.date }}</div>
+          <div class="program"><span> {{ activity.parent }} </span> {{ activity.name }}</div>
         </div>
       </ion-list>
+      <div v-else class="empty-objectives">
+        <ion-icon :icon="cafe" />
+        <p>Vous n'avez pas d'objectifs pour aujourd'hui...</p>
+      </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList } from '@ionic/vue';
+import { IonPage, IonIcon, IonHeader, IonToolbar, IonTitle, IonContent, IonList } from '@ionic/vue';
+import { cafe } from 'ionicons/icons';
 
 interface State {
-  today: Date;
+  today: string;
   activities: [{
     id: number;
     name: string;
-    date: Date;
+    date: string;
     plannedTime: number;
     passedTime: number;
   }] | null;
@@ -36,23 +45,33 @@ interface State {
 
 export default  {
   name: 'DayObjectives',
-  components: { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList },
+  components: { IonPage, IonIcon, IonHeader, IonToolbar, IonTitle, IonContent, IonList },
   data: (): State => {
     return {
-      today: new Date(),
+      today: new Date().toLocaleDateString('fr', {
+        weekday: 'long',
+        month: 'long',
+        day: '2-digit',
+        year: 'numeric'
+      }).replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase()),
       activities: null,
+    }
+  },
+  setup() {
+    return {
+      cafe
     }
   },
   methods: {
     getDayOfWeek(): any {
       // use today date
-      return {
+      const data = {
         response: [
           {
             id: 1,
             parent: 0,
             name: "Lecture Nietzsche",
-            date: new Date(),
+            date: new Date(2021, 2, 16, 8, 0, 0),
             plannedTime: 3600000,
             passedTime: 0
           },
@@ -60,16 +79,33 @@ export default  {
             id: 3,
             parent: 1,
             name: "Musculation Abdos",
-            date: new Date(),
+            date: new Date(2021, 2, 16, 10, 30, 0),
             plannedTime: 1800000,
             passedTime: 0
           }
         ]
       };
+
+      const response = data.response.map((value) => {
+        const newVal: any = {
+          id: value.id,
+          parent: value.parent,
+          name: value.name,
+          plannedTime: value.plannedTime,
+          passedTime: value.passedTime,
+        };
+
+        const hour = value.date.getHours() < 10 ? "0" + value.date.getHours() : value.date.getHours();
+        const minutes = value.date.getMinutes() < 10 ? "0" + value.date.getMinutes() : value.date.getMinutes() ;
+        newVal.date = hour + "H" + minutes;
+
+        return newVal;
+      });
+
+      return response;
     },
     getActivities(): void {
-      const data = this.getDayOfWeek();
-      this.activities = data.response;
+      this.activities = this.getDayOfWeek();
     }
   },
   mounted() {
