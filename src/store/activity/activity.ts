9e2b3229e299c5a8d7e802cb1b58371041bac232
Jérémy Legendre <jeremy.legendre@raccourci.fr>
2021-07-24 00:04:37 +0200

    fix method to filter activities on date

--- src/store/activity/activity.ts
+++ src/store/activity/activity.ts
@@ -25,8 +25,7 @@ const getters = {
     getActivitiesFromDate: (state) => (date) => {
         if (state.activities !== null && state.activities.length > 0) {
             return state.activities.filter((activity) => {
-                const dateDiff = Math.abs(date-activity.date);
-                return dateDiff < 86000000;
+                return activity.date > date && activity.date < (date + 86400000);
             });
         } else {
             return false;
