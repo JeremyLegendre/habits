ee3ec441fcb3e6ded2ea772b032e586e4b5efe55
Jérémy Legendre <jeremy.legendre@raccourci.fr>
2021-07-24 08:56:37 +0200

    podium style

--- src/views/MyAccount.vue
+++ src/views/MyAccount.vue
@@ -28,15 +28,15 @@
         <div class="top-users">
           <div id="second" v-if="topUsers[1]">
             <span>{{topUsers[1].name}}</span>
-            <span>{{topUsers[1].points}}</span>
+            <span class="points">{{topUsers[1].points}}</span>
           </div>
           <div id="first" v-if="topUsers[0]">
             <span>{{topUsers[0].name}}</span>
-            <span>{{topUsers[0].points}}</span>
+            <span class="points">{{topUsers[0].points}}</span>
           </div>
           <div id="third" v-if="topUsers[2]">
             <span>{{topUsers[2].name}}</span>
-            <span>{{topUsers[2].points}}</span>
+            <span class="points">{{topUsers[2].points}}</span>
           </div>
         </div>
       </ion-card>
@@ -107,14 +107,43 @@ ion-card {
 .top-users > div {
   display: flex;
   flex-direction: column;
+  background-color: rgba(255,255,255,0.3);
+  backdrop-filter: blur(10px);
+  border-left: 1px solid rgba(255,255,255,0.3);
+  border-top: 1px solid rgba(255,255,255,0.3);
+  box-shadow: 10px 10px 60px -8px rgba(0,0,0,0.2);
+  padding: 10px 0;
+  font-weight: bold;
+}
+
+.top-users > div > span:last-child {
+  margin-top: 10px;
+}
+
+.top-users > #first {
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+}
+
+.top-users > #first > span:last-child {
+  color: gold;
 }
 
 .top-users > #second {
   margin-top: 30px;
+  border-top-left-radius: 5px;
+}
+
+.top-users > #second > span:last-child {
+  color: #a89c91;
 }
 
 .top-users > #third {
   margin-top: 50px;
+  border-top-right-radius: 5px;
 }
 
+.top-users > #third > span:last-child {
+  color: #CD7F32;
+}
 </style>
